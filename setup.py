311f032ca3f9c528f2df850049d0e390b05a8b47
Siymiel <skinuthia77@gmail.com>
2024-08-10 13:47:18 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sampy",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "colorama",
+        "tqdm"
+    ],
+    entry_points={
+        'console_scripts': [
+            'sampy=sampy.installer:main',
+        ],
+    },
+    author="Samuel Kinuthia",
+    author_email="skinuthia77@gmail.com",
+    description="A package to automate Django package installation and uninstallation.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/mypackage",  # Replace with your GitHub URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
